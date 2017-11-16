d81b93dd1fbf70b18068c303e2905aa1c17ea348
282921012 <282921012@qq.com>
2017-11-16 10:46:49 +0800

    神导修改测试api

--- app/src/main/java/com/cepri/demo/NativeActivity.java
+++ app/src/main/java/com/cepri/demo/NativeActivity.java
@@ -1,11 +1,11 @@
 package com.cepri.demo;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
-import java.util.Arrays;
+import android.widget.TextView;
 
 import cepri.device.utils.SecurityUnit;
 
@@ -17,6 +17,7 @@ import cepri.device.utils.SecurityUnit;
 public class NativeActivity extends AppCompatActivity implements View.OnClickListener {
     SecurityUnit mSecurityUnit = new SecurityUnit();
     private static final String TAG = "Reginer";
+    private TextView mResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +35,10 @@ public class NativeActivity extends AppCompatActivity implements View.OnClickLis
         findViewById(R.id.send).setOnClickListener(this);
         findViewById(R.id.recData).setOnClickListener(this);
         findViewById(R.id.setTimeOut).setOnClickListener(this);
-
+        mResult = findViewById(R.id.result);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -53,19 +55,30 @@ public class NativeActivity extends AppCompatActivity implements View.OnClickLis
                 Log.d(TAG, String.valueOf(mSecurityUnit.ClearRecvCache()));
                 break;
             case R.id.config:
-                Log.d(TAG, String.valueOf(mSecurityUnit.Config(9600, 8, 2, 1, 0)));
+                Log.d(TAG, String.valueOf(mSecurityUnit.Config(115200, 8, 0, 1, 1)));
+                mResult.setText("设置的参数为:115200, 8, 0, 1, 1");
                 break;
             case R.id.send:
-                Log.d(TAG, String.valueOf(mSecurityUnit.SendData(new byte[]{0x16, 0x52}, 0, 2)));
+                byte[] t1 = new byte[]{(byte) 0xE9, 0x00, 0x02, 0x00, 0x01, (byte) 0xEC, (byte) 0xE6};
+                StringBuilder textSend = new StringBuilder();
+                for (byte aT1 : t1) {
+                    textSend.append(String.format("%02x ", aT1));
+                }
+                mResult.setText("发送的数据为:" + textSend);
+                Log.d(TAG, String.valueOf(mSecurityUnit.SendData(t1, 0, 7)));
                 break;
             case R.id.recData:
-                byte[] aa = new byte[2];
-                Log.d(TAG, String.valueOf(mSecurityUnit.RecvData(aa, 0, 2)));
-                Log.d(TAG, "recData: "+ Arrays.toString(aa));
+                byte[] aa = new byte[255];
+                int length = (mSecurityUnit.RecvData(aa, 0, 255));
+                StringBuilder text = new StringBuilder();
+                for (int i = 0; i < length; i++) {
+                    text.append(String.format("%02x ", aa[i]));
+                }
+                mResult.setText("接收的数据为:" + text);
                 break;
             case R.id.setTimeOut:
-                Log.d(TAG, String.valueOf(mSecurityUnit.SetTimeOut(0, 200)));
-                Log.d(TAG, String.valueOf(mSecurityUnit.SetTimeOut(1, 200)));
+                Log.d(TAG, String.valueOf(mSecurityUnit.SetTimeOut(0, 800)));
+                Log.d(TAG, String.valueOf(mSecurityUnit.SetTimeOut(1, 800)));
                 break;
             default:
                 break;
